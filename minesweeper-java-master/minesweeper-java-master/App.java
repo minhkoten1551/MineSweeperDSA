6d8392f2ecd059c4620bd1b76652d512d8717840
lmt26102002 <84053899+minhkoten1551@users.noreply.github.com>
2024-06-20 08:47:19 +0700

    Update App.java

--- minesweeper-java-master/minesweeper-java-master/App.java
+++ minesweeper-java-master/minesweeper-java-master/App.java
@@ -1,3 +1,8 @@
+/* Name: Lê Minh Tuấn 
+IU code: ITITIU20341
+Purpose: To replicate and add extra functions to the original Minesweeper using the knowledges that I has been taught throught out the course.
+*/ 
+A short description of the program/exercise. 
 public class App {
     public static void main(String[] args) throws Exception {
         Minesweeper minesweeper = new Minesweeper();
